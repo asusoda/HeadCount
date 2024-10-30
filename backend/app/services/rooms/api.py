da07ae55318fd772bf27a9bc8ccdf30fc7b2845c
rtwoo <rtwoo@asu.edu>
2024-10-29 19:26:18 -0700

    Updated to new query format

--- backend/app/services/rooms/api.py
+++ backend/app/services/rooms/api.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.database import engine
 from app.services.rooms.models import Room
@@ -9,13 +10,19 @@ router = APIRouter()
 @router.get('')
 async def get_rooms():
     with Session(engine) as session:
-        rooms = session.query(Room).all()
+        rooms = session.scalars(
+            select(Room)
+            ).all()
         return {'rooms': rooms}
 
 @router.get('/{room_id}')
 async def get_room(room_id: int):
     with Session(engine) as session:
-        room = session.query(Room).filter(Room.id == room_id).first()
+        room = session.scalars(
+            select(Room)
+            .filter_by(id=room_id)
+            .limit(1)
+            ).first()
         if room:
             return {'room': room}
         return {'error': 'Room not found'}, 404
