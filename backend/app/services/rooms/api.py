a091c296d6483774ec58c9953ea4d279f268c421
rtwoo <rtwoo@asu.edu>
2024-10-30 17:22:30 -0700

    Add delete endpoint for rooms and update .gitignore

--- backend/app/services/rooms/api.py
+++ backend/app/services/rooms/api.py
@@ -4,8 +4,23 @@ from app.database import engine
 from app.services.rooms.models import Room
 
 from fastapi import APIRouter
+from fastapi import HTTPException, status
 
 router = APIRouter()
+
+@router.delete('/{room_id}')
+async def delete_rooms(room_id: int):
+    with Session(engine) as session:
+        room = session.scalars(
+            select(Room)
+            .filter_by(id=room_id)
+            .limit(1)
+            ).first()
+        if room:
+            session.delete(room)
+            session.commit()
+            return {'success': f'Room {room_id} deleted'}
+        return {'error': 'Room not found'}, 404
 
 @router.get('')
 async def get_rooms():
