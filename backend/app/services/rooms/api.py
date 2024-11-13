b1c88ef03b2362425cd12de7fe0aac5cba8adb2f
revanthBottu <137331859+revanthBottu@users.noreply.github.com>
2024-11-13 16:02:15 -0700

    Added untested room edit endpoint

--- backend/app/services/rooms/api.py
+++ backend/app/services/rooms/api.py
@@ -3,10 +3,60 @@ from sqlalchemy.orm import Session
 from app.database import engine
 from app.services.rooms.models import Room
 
-from fastapi import APIRouter
+from fastapi import APIRouter,FastAPI
 from fastapi import HTTPException, status
 
 router = APIRouter()
+
+# @router.put('/{room_id}')
+# async def edit_room(room_id: int, name: str, max_occupancy: int):
+#     with Session(engine) as session:
+#         room = session.scalars(
+#             select(Room)
+#             .filter_by(id=room_id)
+#             .limit(1)
+#             ).first()
+#         if room:
+#             room.max_occupancy = max_occupancy;
+#             room.name = name;
+#             return {'room': room}
+#         return {'error': 'Room not found'}, 404
+
+@router.put('/{room_id}')
+async def edit_room(room_id: int, newroom: Room):
+    with Session(engine) as session:
+        room = session.scalars(
+            select(Room)
+            .filter_by(id=room_id)
+            .limit(1)
+            ).first()
+        if room:
+            room = newroom
+            session.commit()
+            return {'room': room}
+        return {'error': 'Room not found'}, 404
+
+# @router.put('/{room_id}')
+# async def edit_room(room_id: int, max_occupancy: int):
+#     with Session(engine) as session:
+#         room = session.scalars(
+#             select(Room)
+#             .filter_by(id=room_id)
+#             .limit(1)
+#             ).first()
+#         if room:
+#             room.max_occupancy = max_occupancy;
+#             return {'room': room}
+#         return {'error': 'Room not found'}, 404
+
+@router.post('')
+async def create_room(name: str, max_occupancy: int):
+    with Session(engine) as session:
+        new_room = Room(name=name, max_occupancy = max_occupancy);
+        session.add(new_room);
+        session.commit();
+        return {'success': f'Room {new_room.id} created'}
+
 
 @router.delete('/{room_id}')
 async def delete_rooms(room_id: int):
